da3492a0094e6630444e22bbb24f6e295eb7ed68
michosse <michal.lemanski12@gmail.com>
2024-06-18 15:14:55 +0200

    delete dev info

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {QuestionService} from "./services/question.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {AxiosService} from "./services/axios.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title: string = "Shrooms";
+  data: string = ""
+  constructor(private axiosService: AxiosService) {
+  }
+   ngOnInit() {
+     this.axiosService.request(
+       "GET",
+       "/current-user",
+       null
+     ).then(
+       (response) => this.data += response.data
+     );
+   }
 }
